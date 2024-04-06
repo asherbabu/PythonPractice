6931c677421047ae1450db2cd11ff768abb1788c
asherbabu <88152482+asherbabu@users.noreply.github.com>
2024-04-06 23:52:31 +0530

    QR Code Generator

---
+++ QRcodeGenerator.py
@@ -0,0 +1,13 @@
+import qrcode
+
+def qrcodeimg(link):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+    )
+    qr.add_data(link)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="magenta", back_color="white")
+    img.show()
