f1a77e56c3754b6acd88268b673cb541526506e9
asherbabu <88152482+asherbabu@users.noreply.github.com>
2024-04-25 00:20:33 +0530

    Dice Roll Simulator

--- DiceRollSimulator.py
+++ DiceRollSimulator.py
@@ -1,7 +1,9 @@
 #Dice Roll Simulator
 import random
 num_dice = int(input("Select the number of dice: "))
-outcome = []
+outcomes = []
 for i in range(1, 6*num_dice + 1):
     #print(i)
-    outcome.append(i)
+    outcomes.append(i)
+print(outcomes)
+random.choice(outcomes)
